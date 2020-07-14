c67ba9fa1eb1c6f863b1afd4b876284c5c4b5abe
Yusuke Ohshima <ohshima.yusuke@yukke.org>
2020-07-15 00:51:27 +0900

    add control scripts.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './scss/marquee.scss';
+
+
+class Marquee {
+  constructor(elem) {
+    this.elem= elem;
+  }
+
+  start() {
+    const target = this.elem.querySelector("");
+  }
+
+  setDulation(millisecond) {
+  }
+}
