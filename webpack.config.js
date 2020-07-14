1ebd66c7b2510f0c5826baefda0eb81e9ffd2264
Yusuke Ohshima <ohshima.yusuke@yukke.org>
2020-07-15 00:47:05 +0900

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  entry: "./src/index.js",
   output: {
     //path: path.resolve(__dirname, "output"),
     filename: "bundle.js",
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "babel-loader", options: { presets: ["@babel/preset-env"], } }
+        ]
+      },
       {
         test: /\.scss$/i,
         use: [
